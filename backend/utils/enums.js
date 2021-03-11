c7013d3f8c1a9ed00829c088beff372d2e56b43f
SandeepReddyKandi <kandisandeepreddy98@gmail.com>
2021-03-12 04:53:20 +0530

    Update models and add enums

---
+++ backend/utils/enums.js
@@ -0,0 +1,18 @@
+const currencyEnums = {
+  USD: 'USD',
+  KWD: 'KWD',
+  BHD: 'BHD',
+  GBP: 'GBP',
+  EUR: 'EUR',
+  CAD: 'CAD',
+};
+
+const expenseEnums = {
+  CREATED: 'Created',
+  SETTLED: 'Settled',
+};
+
+module.exports = {
+  currencyEnums,
+  expenseEnums,
+};
