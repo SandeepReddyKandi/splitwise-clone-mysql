b6151088cfed878428b088b8f2999499e4653749
SandeepReddyKandi <kandisandeepreddy98@gmail.com>
2021-03-12 14:19:16 +0530

    group balance page added to group page

--- frontend/src/components/Dashboard/Groups/UserGroup.js
+++ frontend/src/components/Dashboard/Groups/UserGroup.js
@@ -5,20 +5,14 @@ import ExpenseList from './ExpenseList';
 import '../dashboard.css';
 
 const UserGroups = (props)=>{
-    let bool = true;
-    const [linkStaus, getLinkStatus] = useState('');
-
-    // useEffect(()=>{
-
-    // })
-
     const groupName = props.location.groupName;
     const groups = useSelector(state => state.groupExpenses);
     const expList = groups.groups[0].expenses;
     const userExpenses = groups.totalExpenses;
+    const showUsers = userExpenses.slice(0, 1);
+    const remainingUsers = userExpenses.slice(1, userExpenses.length);
 
-    // console.log(userExpenses);
-    console.log(props.location);
+    // console.log(props.location);
 
     return (
         <div className="container user-groups">
@@ -51,12 +45,39 @@ const UserGroups = (props)=>{
                         <div className="col m12 s12 sidebar-header ">
                             <h6 className="grey-text">GROUP BALANCES</h6>
                         </div>
-                        <div className="col m12 s12">
+                        <div className="col m12 s12" id="main-list">
                             <ul className="collection users-collection">
                                 {
-                                    userExpenses.length ?
+                                    showUsers.length ?
                                     (
-                                        userExpenses.map((usr) =>{
+                                        showUsers.map((usr) =>{
+                                            return (
+                                                <li className="collection-item">
+                                                    <div className="row valign-wrapper" style={{marginBottom: "0px"}}>
+                                                        <img className="col m3" src="https://img.icons8.com/fluent/50/000000/user-male-circle.png"/>
+                                                        <div className="col m9 left-align">
+                                                            <h6 style={{marginBottom: "0px"}}>{usr.user}</h6>
+                                                            {
+                                                                usr.amt > 0 ? 
+                                                                    <p className="orange-text" style={{marginTop: "0px"}}>Owes USD {usr.amt}</p> : 
+                                                                    <p className="green-text" style={{marginTop: "0px"}}>Owes USD {-usr.amt}</p>
+                                                            }
+                                                        </div>
+                                                    </div>
+                                                </li>
+                                            )
+                                        })
+                                    ):
+                                    (
+                                        <div>Loading...</div>
+                                    )
+                                }
+                            </ul>
+                            <ul className="collection users-collection vanish" id="extraInfo">
+                                {
+                                    remainingUsers.length ?
+                                    (
+                                        remainingUsers.map((usr) =>{
                                             return (
                                                 <li className="collection-item">
                                                     <div className="row valign-wrapper" style={{marginBottom: "0px"}}>
@@ -82,36 +103,17 @@ const UserGroups = (props)=>{
                         </div>
                     </div>
                     <div className="view-details">
-                        <a href="#" id="openDetailsLink" className="contentLink" onClick={()=>{
-                            getLinkStatus(bool)
-                        }}>View Details...</a>
+                        <a href="#" id="openDetailsLink" className="contentLink row" onClick={()=>{
+                            document.querySelector("#extraInfo").classList.toggle('vanish');
+                            document.querySelector("#openDetailsLink").classList.toggle('vanish');
+                            document.querySelector("#closeDetailsLink").classList.toggle('vanish');
+                        }}><span className="col m12 center-align">view more...</span></a>
 
-                        {
-                            document.querySelector("#extraInfo").classList.toggle('vanish'),
-                            document.querySelector("#openDetailsLink").classList.toggle('vanish')
-                        }
-
-                        <ul id="extraInfo">
-                            {
-                                userExpenses.map((usr)=>{
-                                    return(
-                                        <li className="collection-item">
-                                            <div className="row valign-wrapper" style={{marginBottom: "0px"}}>
-                                                <img className="col m3" src="https://img.icons8.com/fluent/50/000000/user-male-circle.png"/>
-                                                <div className="col m9 left-align">
-                                                    <h6 style={{marginBottom: "0px"}}>{usr.user}</h6>
-                                                    {
-                                                        usr.amt > 0 ? 
-                                                            <p className="orange-text" style={{marginTop: "0px"}}>Owes USD {usr.amt}</p> : 
-                                                            <p className="green-text" style={{marginTop: "0px"}}>Owes USD {-usr.amt}</p>
-                                                    }
-                                                </div>
-                                            </div>
-                                        </li>
-                                    )
-                                })
-                            }
-                        </ul>
+                        <a href="#" id="closeDetailsLink" className="contentLink vanish row" onClick={()=>{
+                            document.querySelector("#extraInfo").classList.toggle('vanish');
+                            document.querySelector("#openDetailsLink").classList.toggle('vanish');
+                            document.querySelector("#closeDetailsLink").classList.toggle('vanish');
+                        }}><span className="col m12 center-align">close</span></a>
                     </div>
                 </div>
             </div>
